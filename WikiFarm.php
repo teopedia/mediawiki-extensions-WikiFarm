f2b0af95a187eabbd4a7a2138e6e04094069da39
pavel.malahov <pavelmalahov@gmail.com>
2018-12-01 18:42:34 +0700

    Removed comments

--- WikiFarm.php
+++ WikiFarm.php
@@ -10,25 +10,3 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 } else {
 	die( 'This version of the WikiFarm extension requires MediaWiki 1.29+' );
 }
-
-
-//-------------------------------------------------
-//------- Register WikiFarm extention
-//-------------------------------------------------
-
-// Register Special page and place it in section "wiki data and tools"
-//$wgSpecialPages['WikiFarm'] = 'SpecialWikiFarm';
-//$wgSpecialPageGroups['WikiFarm'] = 'wiki';
-
-//-------------------------------------------------
-//------- include WikiFarm tools
-//-------------------------------------------------
-//global $IP; //get the variable from LocalSettings.php
-//require_once( "$IP/extensions/WikiFarm/WikiFarmStatistics/WikiFarmStatistics.php" );
-//require_once( "$IP/extensions/WikiFarm/WikiFarmChanges/WikiFarmChanges.php" );
-//require_once( "$IP/extensions/WikiFarm/WikiFarmPages/WikiFarmPages.php" );
-//require_once( "$IP/extensions/WikiFarm/WikiFarmLog/WikiFarmLog.php" );
-//require_once( "$IP/extensions/WikiFarm/WikiFarmUsers/WikiFarmUsers.php" );
-//require_once( __DIR__ . "/WikiFarmLinks.php" );
-//require_once( "$IP/extensions/WikiFarm/WikiFarmSearch/WikiFarmSearch.php" );
-
